5fc76e36ed992c43fdc7b8175fe4db405751caae
github username <github email address>
2017-10-02 23:00:36 -0400

    changed urls

--- src/adapters/auth.js
+++ src/adapters/auth.js
@@ -2,7 +2,7 @@ class Auth {
 
 	static login(userParams) {
 		const userJSON = JSON.stringify(userParams)
-		return fetch('http://localhost:3000/api/v1/login', {
+		return fetch('https://rentroll-api.herokuapp.com/api/v1/login', {
 			method: 'post',
 			body: userJSON,
 			headers: {
@@ -15,7 +15,7 @@ class Auth {
 
 	static signup(userParams) {
 		const userJSON = JSON.stringify(userParams)
-		return fetch('http://localhost:3000/api/v1/users', {
+		return fetch('https://rentroll-api.herokuapp.com/api/v1/users', {
 			method: 'post',
 			body: userJSON,
 			headers: {
@@ -28,70 +28,70 @@ class Auth {
 
 	static me() {
 		const jwtToken = localStorage.getItem("token")
-		return fetch('http://localhost:3000/api/v1/me', {
+		return fetch('https://rentroll-api.herokuapp.com/api/v1/me', {
 			headers: {
 				"Authorization": `Bearer ${jwtToken}`,
 				"Accept": "application/json"
 			}
 		})
 			.then(res => res.json())
-		}
-		
-		static save(userListingParams) {
-			const userJSON = JSON.stringify(userListingParams)
-			const jwtToken = localStorage.getItem("token")
-				return fetch('http://localhost:3000/api/v1/user/listings', {
-				method: 'post',
-				body: userJSON,
-				headers: {
-					"Authorization": `Bearer ${jwtToken}`,
-					"Content-Type": "application/json",
-					"Accept": "application/json"
-				}
-			})
-			.then(res => res.json())
-		}
+	}
 
-		static delete(userListingParams) {
-			const userJSON = JSON.stringify(userListingParams)
-			const jwtToken = localStorage.getItem("token")
-				return fetch(`http://localhost:3000/api/v1/user/listings/${userListingParams}`, {
-				method: 'delete',
-				body: userJSON,
-				headers: {
-					"Authorization": `Bearer ${jwtToken}`,
-					"Content-Type": "application/json",
-					"Accept": "application/json"
-				}
-			})
-			.then(res => res.json())
-		}
-
-			static saveFinancialInfo(userListingParams) {
-				const userJSON = JSON.stringify(userListingParams)
-				const jwtToken = localStorage.getItem("token")
-					return fetch('http://localhost:3000/api/v1/user/financials', {
-					method: 'post',
-					body: userJSON,
-					headers: {
-						"Authorization": `Bearer ${jwtToken}`,
-						"Content-Type": "application/json",
-						"Accept": "application/json"
-					}
-				})
-				.then(res => res.json())
+	static save(userListingParams) {
+		const userJSON = JSON.stringify(userListingParams)
+		const jwtToken = localStorage.getItem("token")
+		return fetch('https://rentroll-api.herokuapp.com/api/v1/user/listings', {
+			method: 'post',
+			body: userJSON,
+			headers: {
+				"Authorization": `Bearer ${jwtToken}`,
+				"Content-Type": "application/json",
+				"Accept": "application/json"
 			}
-			
-		static getFinancialData() {
-			const jwtToken = localStorage.getItem("token")
-			return fetch('http://localhost:3000/api/v1/user/financials', {
-				headers: {
-					"Authorization": `Bearer ${jwtToken}`,
-					"Accept": "application/json"
-				}
-			})
-				.then(res => res.json())
-		}
+		})
+			.then(res => res.json())
+	}
+
+	static delete(userListingParams) {
+		const userJSON = JSON.stringify(userListingParams)
+		const jwtToken = localStorage.getItem("token")
+		return fetch(`https://rentroll-api.herokuapp.com/api/v1/user/listings/${userListingParams}`, {
+			method: 'delete',
+			body: userJSON,
+			headers: {
+				"Authorization": `Bearer ${jwtToken}`,
+				"Content-Type": "application/json",
+				"Accept": "application/json"
+			}
+		})
+			.then(res => res.json())
+	}
+
+	static saveFinancialInfo(userListingParams) {
+		const userJSON = JSON.stringify(userListingParams)
+		const jwtToken = localStorage.getItem("token")
+		return fetch('https://rentroll-api.herokuapp.com/api/v1/user/financials', {
+			method: 'post',
+			body: userJSON,
+			headers: {
+				"Authorization": `Bearer ${jwtToken}`,
+				"Content-Type": "application/json",
+				"Accept": "application/json"
+			}
+		})
+			.then(res => res.json())
+	}
+
+	static getFinancialData() {
+		const jwtToken = localStorage.getItem("token")
+		return fetch('https://rentroll-api.herokuapp.com/api/v1/user/financials', {
+			headers: {
+				"Authorization": `Bearer ${jwtToken}`,
+				"Accept": "application/json"
+			}
+		})
+			.then(res => res.json())
+	}
 
 
 	static logOut() {
